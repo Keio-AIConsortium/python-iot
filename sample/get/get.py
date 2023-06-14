7d79bd866221dfae5269dcff2aa6d6c7658e9c08
hirobon1690 <58695125+hirobon1690@users.noreply.github.com>
2023-06-14 14:59:35 +0900

    Update get.py

--- sample/get/get.py
+++ sample/get/get.py
@@ -6,7 +6,7 @@ def do_connect():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('aterm-520cf8-g', '869734a7a2bd0')
+        sta_if.connect('', '')
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
