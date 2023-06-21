82900c15b5b8b42f508f974e83c48f6928aaed3d
Oyaki <42294158+Oyaki122@users.noreply.github.com>
2023-06-21 13:28:48 +0900

    Update main.py

--- sample/dht11/main.py
+++ sample/dht11/main.py
@@ -2,9 +2,8 @@ from esp_config import *
 from time import sleep
 import dht
 
-d = dht.DHT11(machine.Pin(D5))
+d = dht.DHT11(machine.Pin(D6))
 while True:
     d.measure()
-    print(d.temperature())
-    print(d.humidity())
+    print('temp: ', d.temperature(), 'humidity: ', d.humidity())
     sleep(1)
