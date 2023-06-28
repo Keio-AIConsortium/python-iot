8851728339d4bff30d6d97c4d24934be39e863d3
Oyaki122 <oc.yaki122@gmail.com>
2023-06-28 19:51:37 +0900

    今度こそ時計を追加

---
+++ sample/clock/main.py
@@ -0,0 +1,18 @@
+from machine import RTC
+import utime
+import ntptime
+import wifi
+from time import sleep
+
+wifi.connect()
+rtc = RTC()
+ntptime.settime() # setup time by remote NTP server
+
+def get_jst():
+ tm = utime.localtime(utime.time()) # UTC now
+ jst = str(tm[0])+'/'+str(tm[1])+'/'+str(tm[2])+' '+str((tm[3]+9)%24)+':'+str(tm[4])+':'+str(tm[5])
+ return jst
+
+while True:
+    print(get_jst())
+    sleep(1)
